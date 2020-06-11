a796c753ba2da4de2a0b1e162436bf0027439dd6
Sharad Jadhav <sharad.jadhav.8551@gmail.com>
2020-06-11 19:50:03 +0530

    package.json, eslintrc.js, prettierrc.js modified
    custom scripts added to package.json and eslintrc.js, prettierrc.js modified

--- Client/ReactNative/Message/.prettierrc.js
+++ Client/ReactNative/Message/.prettierrc.js
@@ -1,6 +1,10 @@
 module.exports = {
+  semi: true,
+  tabWidth: 2,
   bracketSpacing: false,
   jsxBracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
+  endOfLine: 'auto',
+  jsxSingleQuote: true,
 };
