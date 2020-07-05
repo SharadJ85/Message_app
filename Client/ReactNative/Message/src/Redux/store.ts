22f92ada8119989ce647b4e382cb4e26755c7669
Sharad Jadhav <sharad.jadhav.8551@gmail.com>
2020-07-05 20:44:10 +0530

    APP: uninstalled redux-saga and added redux-thunk.
    dependencies:
         "redux-thunk": "^2.3.0",

--- Client/ReactNative/Message/src/Redux/store.ts
+++ Client/ReactNative/Message/src/Redux/store.ts
@@ -1,16 +1,10 @@
 import {applyMiddleware, createStore} from 'redux';
-import createSagaMiddleware from 'redux-saga';
+import thunk from 'redux-thunk';
 import allReducers from './Reducers/index';
 import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 
-const sagaMiddleware = createSagaMiddleware();
-
 const myStore = () => {
-  const store = createStore(
-    allReducers,
-    composeWithDevTools(applyMiddleware(sagaMiddleware)),
-  );
-  return store;
+  return createStore(allReducers, composeWithDevTools(applyMiddleware(thunk)));
 };
 
 export default myStore();
