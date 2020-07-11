0c02f64823f239cd430a58b671b2d9da697f7836
Sharad Jadhav <sharad.jadhav.8551@gmail.com>
2020-07-11 07:22:15 +0530

    SERVER: messages sent and received between APP and SERVER.

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express';
 import { createServer } from 'http';
 import { listen, Server } from 'socket.io';
+import { fromAppTypes } from './indexTypes';
 
 // set port number
 const port = process.env.PORT || 4444;
@@ -20,16 +21,53 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/index.html');
 });
 
+const firebaseUserIdList: Array<string> = [];
+
+// on connect
 io.on('connection', (socket) => {
-    console.log('user connected');
-    socket.emit('commEvent', { data: 'connection successful' });
+    // user connected
+    console.log('SERVER-LOG: user connected,socketID==', socket.id);
+
+    // emit communication event saying user connected
+    socket.emit('commEvent', { commEvent: 'SERVER-EMIT: connection successful' });
+
+    // on disconnect
     socket.on('disconnect', () => {
-        console.log('user disconnected');
-        socket.emit('commEvent', { data: 'disconnected' });
+        // user disconnected
+        console.log('SERVER-LOG: user disconnected');
+
+        // emit communication event saying user disconnected
+        socket.emit('commEvent', { data: 'SERVER-EMIT: disconnected' });
+    });
+
+    // new firebase user
+    socket.on('initial-user-auth', (userFirebaseID: string) => {
+        // log firebase id
+        console.log('SERVER-LOG: initial-user-auth-firebaseID==', userFirebaseID);
+
+        // push to firebase userID list
+        firebaseUserIdList.push(userFirebaseID);
+
+        // firebase userID list
+        console.log('firebaseUserIdList==', firebaseUserIdList);
+    });
+
+    socket.on('APP:new-message', (fromApp: fromAppTypes) => {
+        // log new message data from app
+        console.log('SERVER-LOG: fromAPP==', fromApp);
+
+        // emit new message
+        socket.emit('SERVER:new-message', {
+            userFirebaseID: fromApp.userFirebaseID,
+            recipientFirebaseID: fromApp.recipientFirebaseID,
+            timeStamp: fromApp.timeStamp,
+            message: fromApp.message,
+        });
+        console.log('emit from server');
     });
 });
 
 // listen server updates on specified port
 server.listen(port, () => {
-    console.log('Message app server listening on port:', port);
+    console.log('SERVER-LOG: Message app server listening on port:', port);
 });
---
+++ Server/src/indexTypes.ts
@@ -0,0 +1,6 @@
+export interface fromAppTypes {
+    userFirebaseID: string;
+    recipientFirebaseID: string;
+    timeStamp: string;
+    message: string;
+}
